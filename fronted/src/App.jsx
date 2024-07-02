120c7e98e5d35d351abab376dc6af747c98f7a5b
sarvottam <kumarpurush2511@gmail.com>
2024-07-02 19:13:23 +0530

    setup the fronted side dependencies

---
+++ fronted/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='text-red-500 bg-blue-400 p-2'>
+      Hello Sarvottam kumar !
+    </div>
+  )
+}
+
+export default App
