67da8ffdce88a976919f491e0e1822f6b718b021
sarvottam <kumarpurush2511@gmail.com>
2024-07-03 16:25:54 +0530

    completed the UI design of fronted

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import LoginPage from './Pages/LoginPage'
+import SignUpPage from './Pages/SignUpPage'
+import HomePage from './Pages/HomePage'
+import ExplorePage from './Pages/ExplorePage'
+import LikesPage from './Pages/LikesPage'
+
 
 function App() {
 
   return (
-    <div className='text-red-500 bg-blue-400 p-2'>
-      Hello Sarvottam kumar !
+    <div className='flex '>
+    <Sidebar/>
+    <div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
+      <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='/explore' element={<ExplorePage />} />
+      <Route path='/likes' element={<LikesPage />} />
+      </Routes>
+    </div>
+
     </div>
   )
 }
---
+++ fronted/src/Pages/LoginPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaUnlockAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function LoginPage() {
+  return (
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+      <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 hover:bg-gray-600/10 border-gray-800 text-white">
+        <div className="p-6 spacey-y-4 md:space-y-6 sm:p-8">
+          <h1 className="text-xl font-bold md:text-2xl text-center">
+            Login to your Account 
+          </h1>
+          <button
+            type="button"
+            className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4	focus:ring-[#24292F]/50 font-medium rounded-lg flex gap-2 p-2 items-center w-full text-center justify-center"
+          >
+            <FaGithub className="w-5 h-5" />
+            Login with Github
+          </button>
+          <p className="text-sm font-light text-gray-500">
+            Don't have an account ?{" "}
+            <Link
+              to={"/signup"}
+              className="font-medium text-primary-600 hover:underline text-blue-600"
+            >
+              Signup
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
