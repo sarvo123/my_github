ca27f4c041d5e6f2b94ce5fc47f2ee09039e9716
sarvottam <kumarpurush2511@gmail.com>
2024-07-06 00:17:15 +0530

    completed the build command

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,29 @@ import exploreRoutes from "./routes/explore.Route.js"
 import connectToDb from './db/connectToDb.js';
 import passport from 'passport';
 import session from 'express-session';
+import path from 'path';
 
 
 dotenv.config();
 const app = express();
 app.use(cors());
+
+const PORT = process.env.PORT || 5000; 
+const __dirname = path.resolve();
+console.log("dirname" , __dirname);
+
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 app.use(session({ secret: `${process.env.SECRET_KEY}`, resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(express.static(path.join(__dirname , "/fronted/dist")))
+
+app.get("*" , (req,res)=>{
+    res.sendFile(path.join(__dirname , "fronted","dist","index.html"));
+})
+
 
 app.get("/" , (req,res)=>{
     res.send("server is ready ");
@@ -32,7 +45,7 @@ app.use("/api/users",userRoutes);
 app.use("/api/explore", exploreRoutes);
 
 
-app.listen(5000 , ()=>{
+app.listen(PORT , ()=>{
     connectToDb();
-    console.log("Server started  on PORT : 5000");
+    console.log(`Server started  on PORT : ${PORT}`);
 });
